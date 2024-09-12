c01a67d35de1db88f5c26fa86ca15b28ce67b95c
Seyoung <seyoung5157@gmail.com>
2024-09-12 19:17:14 +0900

    socket 서버 생성 및 연결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
 // nodemon.json에서 ignore 설정을 통해 현재 파일(프론트엔드 부분)이 수정되더라도 서버는 재시작되지 않음
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("서버에 연결되었습니다.");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("메세지 : ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("서버 연결이 해제되었습니다.");
+});
+
+setTimeout(() => {
+  socket.send("이 메세지는 10초 뒤에 실행됩니다.");
+}, 10000); // 10초
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
+
 const app = express();
 
 app.set('view engine', "pug");
@@ -10,4 +13,20 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/")); // 다른 주소가 입력되어도 홈으로 복귀
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// const handleListen = () => console.log(`Listening on ws://localhost:3000`);
+
+const server = http.createServer(app); // express를 사용하여 http 서버 생성
+const wss = new WebSocket.Server({server}); // ws를 사용하여 http 서버 위에 WebSocket 서버 생성. (wss = WebSocket Secure)
+
+wss.on("connection", (socket) => {
+  console.log("브라우저에 연결되었습니다.");
+  socket.on("close", () => { // 인터넷 창이 꺼졌을때
+    console.log("브라우저 연결이 해제되었습니다.");
+  });
+  socket.on("message", (message) => {
+    console.log(message.toString()); // WebSocket에서 전송한 메세지를 제대로 인코딩하지 않으면 buffer 형식으로 표시 -> 문자열로 변환 필요
+  });
+  socket.send("hello!");
+});
+
+server.listen(3000, handleListen); // server 객체를 사용하여 http와 webSocket 모두 같은 포트에서 연결할 수 있도록 함
