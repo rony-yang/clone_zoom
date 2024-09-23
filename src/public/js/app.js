2dc8c5865c1b4d42783f60f2219e500010b1f175
Seyoung <seyoung5157@gmail.com>
2024-09-23 15:03:25 +0900

    채팅 1차 완성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,19 +1,52 @@
 // nodemon.json에서 ignore 설정을 통해 현재 파일(프론트엔드 부분)이 수정되더라도 서버는 재시작되지 않음
 
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// JSON 객체를 받아서 string으로 반환
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+};
 
 socket.addEventListener("open", () => {
   console.log("서버에 연결되었습니다.");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("메세지 : ", message.data);
+  // 메세지를 받으면 ul안의 li안에 내용을 넣어서 화면에 표시
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("서버 연결이 해제되었습니다.");
 });
 
-setTimeout(() => {
-  socket.send("이 메세지는 10초 뒤에 실행됩니다.");
-}, 10000); // 10초
+// setTimeout(() => {
+//   socket.send("이 메세지는 10초 뒤에 실행됩니다.");
+// }, 10000); // 10초
+
+// 메세지 입력 후 send 버튼 입력 시
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value)); // 프론트에서 백으로 전송
+  // const li = document.createElement("li");
+  // li.innerText = `You : ${input.value}`;
+  // messageList.append(li);
+  input.value = "";
+};
+
+// 닉네임 입력 후 save 버튼 입력 시
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value)); // text 대신 JSON object 전체를 전송
+};
+
+messageForm.addEventListener("submit", handleSubmit); // 메세지 입력 후 send 버튼 입력 시
+nickForm.addEventListener("submit", handleNickSubmit); // 닉네임 입력 후 save 버튼 입력 시
--- src/server.js
+++ src/server.js
@@ -18,15 +18,29 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app); // express를 사용하여 http 서버 생성
 const wss = new WebSocket.Server({server}); // ws를 사용하여 http 서버 위에 WebSocket 서버 생성. (wss = WebSocket Secure)
 
+const sockets = []; // 서버에 연결된 사람을 확인하기 위해 생성
+
 wss.on("connection", (socket) => {
+  sockets.push(socket); // 누군가 연결하면 sockets 배열에 넣어준다
+  socket["nickname"] = "익명"; // 닉네임이 없을 경우 익명 으로 표시
   console.log("브라우저에 연결되었습니다.");
   socket.on("close", () => { // 인터넷 창이 꺼졌을때
     console.log("브라우저 연결이 해제되었습니다.");
   });
-  socket.on("message", (message) => {
-    console.log(message.toString()); // WebSocket에서 전송한 메세지를 제대로 인코딩하지 않으면 buffer 형식으로 표시 -> 문자열로 변환 필요
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      // 받은 내용이 메세지일때
+      case "new_message" :
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname} : ${message.payload}`)); // 연결된 모든 socket에 접근하여 메세지 전송
+        break;
+      // 받은 내용이 닉네임일때
+      case "nickname" :
+        socket["nickname"] = message.payload; // 받아온 닉네임을 소켓으로 넣어준다
+        break;
+    }
   });
-  socket.send("hello!");
+  //socket.send("hello!");
 });
 
 server.listen(3000, handleListen); // server 객체를 사용하여 http와 webSocket 모두 같은 포트에서 연결할 수 있도록 함
